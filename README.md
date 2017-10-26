882a362ae8b4f66dae672dc0a581f91978c1da33
Jonas Ritz <j.ritz@fz-juelich.de>
2017-10-26 16:03:43 +0200

    Frederik ist en Spast

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Stammbaum
 Stammbaumgenerator 2017, Software Engineering Projekt, Forschungszentrum Jülich GmbH
+
+https://www.dpunkt.de/java/Programmieren_mit_Java/Oberflaechenprogrammierung/3.html
+https://docs.oracle.com/javase/tutorial/uiswing/components/index.html
--- src/application/stammbaum/Main.java
+++ src/application/stammbaum/Main.java
@@ -1,5 +1,28 @@
-public class Main{
+package application.stammbaum;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Main extends JFrame{
   public static void main(String argv[]){
-	  System.out.println("Hallo");
+	  	//Fenster f1 = new Fenster("Stammbaumgenerator");
+	  	JFrame f = new JFrame();
+	  	JButton b = new JButton("bdmkfm");
+	  	f.add(b);
+	  	f.setVisible(true);
+	  	f.setSize(500, 500);
+	  	b.setIcon(UIManager.getIcon("FileView."));
+	  	
+	  	b.addActionListener(new ActionListener(){
+	  		public void actionPerformed(ActionEvent e){
+	  			System.out.println("dsfd");
+	  			f.removeAll();
+	  		}
+	  	});  	
+  }
+  
+  public static void starte(){
+	  	
   }
 }
