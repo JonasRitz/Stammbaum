86d6b42422c377ec75d39bbedcf9f1a7424ae124
JThoennissen <j.thoennissen@fz-juelich.de>
2018-01-28 20:36:20 +0100

    Beziehungen anzeigen klappt so semi

--- src/application/stammbaum/gui/CentralFrame.java
+++ src/application/stammbaum/gui/CentralFrame.java
@@ -31,7 +31,8 @@ public class CentralFrame extends JPanel implements Printable {
 	private Mainscreen parent;
 	private HashMap<Person, JLabel> persons;
 	boolean draw;
-	private HashMap<Point, Point> positionen;
+	private HashMap<Person, Point> positionen;
+	private HashMap<Person, Point[]> verbindungen;
 
 	public CentralFrame(Mainscreen parent, Stammbaum baum){
 		this.draw = false;
@@ -39,6 +40,7 @@ public class CentralFrame extends JPanel implements Printable {
 		this.parent = parent;
 		this.persons = new HashMap<>();
 		this.positionen = new HashMap<>();
+		this.verbindungen = new HashMap<>();
 		this.setVisible(true);
 	}
 	
@@ -58,10 +60,10 @@ public class CentralFrame extends JPanel implements Printable {
 			}
 			personLabel.setBounds(insets.left+zufallX, insets.top+zufallY, jlabelsize.width, jlabelsize.height);
 			this.add(personLabel);
+			
 		}
 		JLabel head = new JLabel("Jeder mit Jedem irgendwie");
-		//head.setFont(head.getFont().deriveFont(40.0f));
-		Dimension headsize = head.getSize(); 
+		Dimension headsize = head.getPreferredSize(); 
 		head.setBounds(insets.left+(sizeOfJPanel.width/2), insets.top+25, headsize.width, headsize.height);
 		this.add(head);
 		this.setVisible(true);
@@ -71,6 +73,8 @@ public class CentralFrame extends JPanel implements Printable {
 
 	protected void refreshAll(Stammbaum baum){
 		this.removeAll();
+		this.positionen.clear();
+		this.verbindungen.clear();
 		boolean hatPlatz = personenOhneBeziehungHinzufuegen(baum);
 		personenMitBeziehungHinzufuegen(baum);
 		this.setVisible(true);
@@ -81,34 +85,13 @@ public class CentralFrame extends JPanel implements Printable {
 	public void personenMitBeziehungHinzufuegen(Stammbaum baum){
 		ArrayList<Person[]> heads = null;
 		if (baum.beziehungen.size() > 0) {
-			//ArrayList<Person[]> ersteZeile = calculateHead(baum);
 			ArrayList<Person[]> ersteZeile = baum.getHead();
 			zeigeErsteZeile(ersteZeile);
 			zeigeNaechsteZeile(baum, ersteZeile, 1); //Start der Rekursion
-			/*
-			ArrayList<Person[]> orig = calculateHead(baum);
-			heads = new ArrayList<Person[]>(orig.size());
-			for (Person[] item : orig) {
-				heads.add(item.clone());
-			}
 			this.draw = true;
-			int anzahl = heads.size();
-			Insets insets = this.getInsets();
-			Dimension sizeOfJPanel = this.getSize();
-			for(Person[] p : heads){
-				JLabel personLabel_v = createJLabelOfPerson(p[0]);
-				JLabel personLabel_m = createJLabelOfPerson(p[1]);
-				Dimension size = personLabel_v.getPreferredSize();
-				personLabel_v.setBounds(insets.left+10, 10 + insets.top, size.width, size.height);
-				personLabel_m.setBounds(insets.left+10+2*size.width, 10 + insets.top, size.width, size.height);
-				this.add(personLabel_v);
-				this.add(personLabel_m);
-				this.positionen.put(new Point(insets.left+10+size.width, 10+insets.top+50), new Point(insets.left+10+2*size.width, 10+insets.top+50));
-				System.out.println("Person mit Beziehung wird optisch hinzugefuegt: " + p[0].toString());
-				System.out.println("Person mit Beziehung wird optisch hinzugefuegt: " + p[1].toString());			
-			}*/
 		}
 		this.setVisible(true);
+		this.paintComponent(this.getGraphics());
 		this.parent.repaint();
 		this.parent.setVisible(true);
 	}
@@ -125,6 +108,8 @@ public class CentralFrame extends JPanel implements Printable {
 			mutter.setBounds(insets.left + distanz_zwischen_paaren*(i+1) + jlabelsize.width*2*i + jlabelsize.width, insets.top + 15, jlabelsize.width, jlabelsize.height);
 			this.add(vater);
 			this.add(mutter);
+			this.positionen.put(ersteZeile.get(i)[0], new Point(insets.left + distanz_zwischen_paaren*(i+1) + jlabelsize.width*2*i, insets.top + 15));
+			this.positionen.put(ersteZeile.get(i)[1], new Point(insets.left + distanz_zwischen_paaren*(i+1) + jlabelsize.width*2*i + jlabelsize.width, insets.top + 15));
 			vater.repaint();
 			mutter.repaint();
 		}
@@ -164,7 +149,7 @@ public class CentralFrame extends JPanel implements Printable {
 		ArrayList<Person[]> paare = new ArrayList<>();
 		int anzPaare = 0;
 		int anzPers = 0;
-		int i =0;
+		int i = 0;
 		for(Person paar[] : paareZeileDruber){
 			for(Person p: baum.getKinderZuEltern(paar)){
 				if(baum.getBeziehungsPartner(p) != null && !schonGehabt.contains(baum.getBeziehungsPartner(p))){
@@ -174,6 +159,21 @@ public class CentralFrame extends JPanel implements Printable {
 					y.setBounds(insets.left + abstand*(i+1) + jlabelsize.width*(anzPaare*2+anzPers) + jlabelsize.width, insets.top + zeile*jlabelsize.height + (zeile+1)*15, jlabelsize.width, jlabelsize.height);
 					this.add(x);
 					this.add(y);
+					this.positionen.put(p, new Point(insets.left+abstand*(i+1)+jlabelsize.width*(anzPaare*2+anzPers)+jlabelsize.width/2, insets.top+zeile*jlabelsize.height+(zeile+1)*15));
+					this.positionen.put(baum.getBeziehungsPartner(p), new Point(insets.left+abstand*(i+1)+jlabelsize.width*(anzPaare*2+anzPers)+jlabelsize.width+jlabelsize.width/2, insets.top+zeile*jlabelsize.height+(zeile+1)*15));
+					
+					Point eltern1 = this.positionen.get(paar[0]);
+					Point eltern2 = this.positionen.get(paar[1]);
+					if (eltern1.x > eltern2.x) {
+						eltern1.setLocation(eltern1.x, eltern1.y+jlabelsize.height);
+						Point[] punkte = {this.positionen.get(p), eltern1};
+						this.verbindungen.put(p, punkte);
+					} else {
+						eltern2.setLocation(eltern2.x, eltern2.y+jlabelsize.height);
+						Point[] punkte = {this.positionen.get(p), eltern2};
+						this.verbindungen.put(p, punkte);
+					}
+					
 					x.repaint();
 					y.repaint();
 					Person[] neuesPaar = {p, baum.getBeziehungsPartner(p)};
@@ -184,8 +184,22 @@ public class CentralFrame extends JPanel implements Printable {
 					i++;
 				}else if(baum.getBeziehungsPartner(p) == null){
 					JLabel x = createJLabelOfPerson(p);
-					x.setBounds(insets.left + abstand*(i+1) + jlabelsize.width*(anzPaare*2+anzPers), insets.top + zeile*jlabelsize.height + (zeile+1)*15, jlabelsize.width, jlabelsize.height);
+					x.setBounds(insets.left+abstand*(i+1)+jlabelsize.width*(anzPaare*2+anzPers), insets.top+zeile*jlabelsize.height+(zeile+1)*15, jlabelsize.width, jlabelsize.height);
 					this.add(x);
+					this.positionen.put(p, new Point(insets.left + abstand*(i+1) + jlabelsize.width*(anzPaare*2+anzPers)+jlabelsize.width/2, insets.top + zeile*jlabelsize.height + (zeile+1)*15));
+					
+					Point eltern1 = this.positionen.get(paar[0]);
+					Point eltern2 = this.positionen.get(paar[1]);
+					if (eltern1.x > eltern2.x) {
+						eltern1.setLocation(eltern1.x, eltern1.y+jlabelsize.height);
+						Point[] punkte = {this.positionen.get(p), eltern1};
+						this.verbindungen.put(p, punkte);
+					} else {
+						eltern2.setLocation(eltern2.x, eltern2.y+jlabelsize.height);
+						Point[] punkte = {this.positionen.get(p), eltern2};
+						this.verbindungen.put(p, punkte);
+					}
+					
 					x.repaint();
 					anzPers++;
 					schonGehabt.add(p);
@@ -200,12 +214,10 @@ public class CentralFrame extends JPanel implements Printable {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         if (this.draw) {
-        	for (Point p : this.positionen.keySet()) {
-        		g.drawLine(p.x, p.y, this.positionen.get(p).x, this.positionen.get(p).y);
-            	this.repaint();
+        	for (Person p : this.verbindungen.keySet()) {
+        		g.drawLine(this.verbindungen.get(p)[0].x, this.verbindungen.get(p)[0].y, this.verbindungen.get(p)[1].x, this.verbindungen.get(p)[1].y);
         	}
         }
-
     }
 	
 	public boolean personenOhneBeziehungHinzufuegen(Stammbaum baum){
@@ -228,39 +240,6 @@ public class CentralFrame extends JPanel implements Printable {
 			}
 		}
 		return hatMehrPlatz;
-	}
-
-	protected ArrayList<Person[]> calculateHead(Stammbaum baum) {
-		ArrayList<Person[]> heads = new ArrayList<>();
-		Person head_v = baum.beziehungen.get(0).vater;
-		Person head_m = baum.beziehungen.get(0).mutter;
-		Person[] head = {head_v, head_m};
-		heads.add(head);
-		for (Beziehung b: baum.beziehungen) {
-			for (Person p : b.kinder) {
-				boolean found = false;
-				for (Person[] paar: heads) {
-					if (paar[0] == p || paar[1] == p) {
-						heads.remove(paar);
-						Person[] new_head = {b.vater, b.mutter};
-						heads.add(new_head);
-						found = true;
-						break;
-					}
-				}
-				if (found == false) {
-					if (!(b.vater == head[0] && b.mutter == head[1])) {
-						Person[] new_head = {b.vater, b.mutter};
-						heads.add(new_head);
-					}
-				}
-			}
-		}
-		return heads;
-	}
-	
-	protected Insets calculateInsets(Insets old){
-		return null;
 	}
 
 	protected JLabel createJLabelOfPerson(Person p){
@@ -385,80 +364,4 @@ public class CentralFrame extends JPanel implements Printable {
 		}
         return PAGE_EXISTS;
 	}
-
-	// Person hinzufuegen
-	/*
-		protected void addPerson(Person p) {
-			// Bild auslesen
-			String src = p.getImageSource();
-			// Infos auslesen
-			String infos = "<html>" + p;
-			if (p.getGeburtsdatum() != null) {
-				infos += "<p>*" + p.getGeburtsdatum().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) + "</p>";
-			}
-			if (p.getSterbedatum() != null) {
-				infos += "<p>†" + p.getSterbedatum().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) + "</p>";
-			}
-			infos += "<html>";
-			// Label erstellen
-			JLabel label = new JLabel(infos, SwingConstants.CENTER);
-			label.setMaximumSize(new Dimension(200, 100));
-			label.setMinimumSize(new Dimension(200, 100));
-			label.setPreferredSize(new Dimension(200, 100));
-			label.setVerticalTextPosition(JLabel.CENTER);
-			label.setHorizontalTextPosition(JLabel.RIGHT);
-			label.setBorder(new LineBorder(Color.BLACK, 1, true));
-			if (src != null) {
-				ImageIcon icon = this.setIcon(src);
-				label.setIcon(icon);
-				label.setIconTextGap(10);
-			}
-			this.persons.put(p, label);
-			this.parent.setVisible(true);
-		}
-
-		protected void removePerson(Person p) {
-			for (Person pers: this.persons.keySet()) {
-				if (pers == p) {
-					this.remove(this.persons.get(p));
-					this.persons.remove(p);
-					break;
-				}
-			}
-			this.parent.repaint();
-		}
-
-	// Beziehung hinzufuegen
-		/*
-		protected void addRelation(Beziehung b) {
-			this.draw = true;
-			this.b = b;
-			for (Person kind: b.kinder) {
-	    		JLabel label = this.persons.get(kind);
-	    		this.box2.add(label);
-	    		this.box2.add(Box.createRigidArea(new Dimension(100,25)));
-	    		this.box1.remove(label);
-	    	}
-			this.paintComponent(this.getGraphics());
-			this.parent.repaint();
-			this.parent.setVisible(true);
-		}*/
-
-
-	/*@Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        if (this.draw) {
-
-        	Point pos_v = this.persons.get(b.vater).getLocation();
-    		Point pos_m = this.persons.get(b.mutter).getLocation();
-    		if (pos_v.x > pos_m.x) {
-    			g.drawLine(pos_v.x, pos_v.y+50, pos_m.x, pos_m.y+50);
-    		} else {
-    			g.drawLine(pos_v.x, pos_v.y+50, pos_m.x, pos_m.y+50);
-    		}
-        	this.repaint();
-        }
-
-    }*/
 }
