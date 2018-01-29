47d72b81061135a05ddace0a95ef26b6fa1c8992
Jonas Ritz <j.ritz@fz-juelich.de>
2018-01-29 13:57:07 +0100

    d

--- src/application/stammbaum/Main.java
+++ src/application/stammbaum/Main.java
@@ -3,13 +3,39 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
 public class Main extends JFrame{
   public static void main(String argv[]){
 	  erstelle_fenster();
   }
   
   public static void erstelle_fenster(){
-	  Mainscreen f = new Mainscreen();
+	  //Mainscreen f = new Mainscreen();
+	  EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Startfenster1 frame = new Startfenster1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
   }
 }
